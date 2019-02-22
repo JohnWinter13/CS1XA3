c9b731e52afaaae5ce6b0901e6d309232bc2ada2
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-02-22 15:16:28 -0500

    Add filetype count functionality for Project01

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -6,6 +6,17 @@ if [ $# -gt 0 ] ; then
         if [ "$arg" = "todo" ] ; then
             grep -r -n "#TODO" --exclude="project_analyze.sh" --exclude="todo.log" > todo.log
             echo "Created todo.log success"
+        elif [ "$arg" = "typecount" ] ; then
+            echo -n 'HTML: '
+            find . -type f -name \*.html | wc -l
+            echo -n 'JavaScript: '
+            find . -type f -name \*.js | wc -l
+            echo -n 'CSS: '
+            find . -type f -name \*.css | wc -l
+            echo -n 'Haskell: '
+            find . -type f -name \*.hs | wc -l
+            echo -n 'Shell: '
+            find . -type f -name \*.sh | wc -l
         fi
     done
 else
