33caad87df2c5d853eac87b77d5606477f03ca0f
Youssef Rizkalla <rizkally@mcmaster.ca>
2019-03-15 19:05:11 +0000

    Modify Lab07 django cors settings

--- Lab07/django_login/django_login/settings.py
+++ Lab07/django_login/django_login/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'auth.apps.AuthConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'django_login.wsgi.application'
 
