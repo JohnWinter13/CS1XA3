6c3e46a49019c96e8f793c055585a251f0392425
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-04-24 01:55:06 -0400

    Override django's JSON serializer to help make parsing in elm easier

--- Project03/django_project/threads/views.py
+++ Project03/django_project/threads/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse, HttpResponse
-from django.core import serializers
+from django.core.serializers.json import Serializer as Builtin_Serializer
 from .models import Thread
 import json
+
+class JSONSerializer(Builtin_Serializer):
+    def get_dump_object(self, obj):
+        return self._current
 
 def add_thread(request):
     thread    = json.loads(request.body)
@@ -20,7 +24,7 @@ def add_thread(request):
     return HttpResponse("Failure")
 
 def get_threads(request):
-    threads_json = serializers.serialize("json", Thread.objects.all())
+    threads_json = JSONSerializer().serialize(Thread.objects.all())
     struct = json.loads(threads_json) 
     data = {"threads": struct}
     return JsonResponse(data)
@@ -29,7 +33,7 @@ def get_thread(request):
     json_req = json.loads(request.body)
     thread_id = json_req.get('id', 0)
     thread = Thread.objects.filter(pk=thread_id)
-    thread_json = serializers.serialize("json", thread)
+    thread_json = JSONSerializer().serialize(thread)
     struct = json.loads(thread_json) 
     data = {"thread" : struct}
     return JsonResponse(data)
