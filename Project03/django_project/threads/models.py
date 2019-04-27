24a8742cebbaed4f925bc6f609e99894a6495af0
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-04-26 22:43:22 -0400

    Update threads model to include subs

--- Project03/django_project/threads/models.py
+++ Project03/django_project/threads/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Sub(models.Model):
+    name        = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=1000)
+
 class Thread(models.Model):
     title     = models.CharField(max_length=300, null=True)
     is_master = models.BooleanField() # True if this post is the first in the thread
@@ -7,3 +11,4 @@ class Thread(models.Model):
     content   = models.CharField(max_length=5000)
     user      = models.CharField(max_length=100) # name of the user who started the thread
     parent    = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    sub       = models.ForeignKey('sub', on_delete=models.CASCADE)
